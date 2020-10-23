c08da9594744f4dc16137e3b6a85c1e8c79d45d3
angelriera91 <angelriera91@gmail.com>
2020-10-23 18:28:44 +0200

    Atributos, constructor, y metodo mostrartodo de Profesional, creado Main para test.

---
+++ Main.ts
@@ -0,0 +1,11 @@
+import {Professional} from "./Professional"
+
+let profesional1 = new Professional('Angel',29,'Hombre',84,174,'Castaño','Marron','Blanco',false,'Español',12,'Actor')
+let profesional2 = new Professional('David',37,'Hombre',84,179,'Castaño','Marron','Blanco',true,'Frances',24,'Doblador')
+let profesional3 = new Professional('Mikel',21,'Hombre',72,179,'Rubio','Azul','Blanco',false,'Español',24,'Maquillador')
+let profesional4 = new Professional('Maria',31,'Mujer',54,171,'Negro','Verdes','Blanco',false,'Alemana',24,'Directora')
+
+profesional1.mostrarTodo();
+profesional2.mostrarTodo();
+profesional3.mostrarTodo();
+profesional4.mostrarTodo();
--- Professional.ts
+++ Professional.ts
@@ -1,4 +1,67 @@
 export class Professional{
 
+    public name: string;
+    public age: number;
+    public genre: string;
+    public weight: number;
+    public height: number;
+    public hairColor: string;
+    public eyeColor: string;
+    public race: string;
+    public isRetired: boolean;
+    public nationality: string;
+    public oscarsNumber: number;
+    public profession: string;
+
+    constructor(name:string,age:number,genre:string, weight: number, height:number,hairColor:string, eyeColor:string, race:string, isRetired:boolean,nationality:string,
+                oscarNumber:number,profession:string){
+
+                    this.name = name;
+                    this.age = age;
+                    this.genre = genre;
+                    this.weight = weight;
+                    this.height = height;
+                    this.hairColor = hairColor;
+                    this.eyeColor = eyeColor;
+                    this.race = race;
+                    this.isRetired = isRetired;
+                    this.nationality = nationality;
+                    this.oscarsNumber = oscarNumber;
+                    this.profession = profession;
+
+    }
+
+    mostrarTodo(){
+
+        let prof: string = ''
+        let status: String;
+
+        if(this.isRetired == true){
+            status = 'Si, esta viviendo la vida'
+        }
+        else{
+            status='No, sigue en activo'
+        }
+
+
+        prof += `
+                Nombre: ${this.name},
+                Edad: ${this.age},
+                genero: ${this.genre},
+                peso: ${this.weight} kg,
+                altura: ${this.height} cm,
+                Color de pelo: ${this.hairColor},
+                Color de ojos: ${this.eyeColor},
+                Raza: ${this.race},
+                ¿Esta Retirado?: ${status},
+                Nacionalidad: ${this.nationality},
+                Numero de Oscars: ${this.oscarsNumber},
+                Profesion: ${this.profession}\n `
+
+        console.log(prof)
+
+    }
+
 
 }
+
